11e27120d2b03fbae6027c51d59ee6c96d758a7a
PRISCILLA URIBE <puribe0@student.ubc.ca>
2023-08-14 14:03:43 -0400

    structure done, working on style

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,7 +3,17 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+    question = params[:question]
+    @answer = coach_answer(question)
+  end
+
+  def coach_answer(question)
+    if question == "I am going to work"
+      "Great!"
+    elsif question.ends_with? "?"
+      "Silly question, get dressed and go to work!"
+    else
+      "I don't care, get dressed and go to work!"
+    end
   end
 end
-
-#do i need another method?
